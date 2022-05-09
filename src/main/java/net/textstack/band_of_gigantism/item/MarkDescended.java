8eb1c205395853c1e65ae6ae641231a3a09debdb
textstack <textstack@protonmail.com>
2022-05-09 13:24:35 -0700

    fix #8

--- src/main/java/net/textstack/band_of_gigantism/item/MarkDescended.java
+++ src/main/java/net/textstack/band_of_gigantism/item/MarkDescended.java
@@ -90,10 +90,11 @@ public class MarkDescended extends Item implements ICurioItem {
             return;
         }
 
-        //try to remove the posY tag for the next equip (this can't be done in onUnequip for some reason)
-        LivingEntity theDude = (LivingEntity) entityIn; //responsible variable names for a responsible developer
-        if (!CurioHelper.hasCurio(theDude, ModItems.MARK_DESCENDED.get())) {
-            stack.getOrCreateTag().remove("posY");
+        //update mark's pos when not equipped
+        LivingEntity living = (LivingEntity) entityIn;
+        if (!CurioHelper.hasCurio(living, ModItems.MARK_DESCENDED.get())) {
+            int posY = (int) Math.ceil(living.blockPosition().getY());
+            this.setPosY(stack, posY);
         }
     }
 
