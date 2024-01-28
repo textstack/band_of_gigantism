4fd2b8484cf12d7d03be9d1c4b3b4bb0e6fec4e8
ChromaNyan <nyancatz593@gmail.com>
2024-01-27 19:13:31 -0600

    thread safety (oops)

--- src/main/java/net/textstack/band_of_gigantism/util/MarkHelper.java
+++ src/main/java/net/textstack/band_of_gigantism/util/MarkHelper.java
@@ -3,10 +3,11 @@ package net.textstack.band_of_gigantism.util;
 import net.textstack.band_of_gigantism.item.base.MarkItem;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class MarkHelper {
-    private static final List<MarkItem> marks = new ArrayList<>();
+    private static final List<MarkItem> marks = Collections.synchronizedList(new ArrayList<>());
 
 
     public static List<MarkItem> getMarks() {
